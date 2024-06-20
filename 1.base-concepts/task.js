9ec0a853201329cb75fa503447a8248f2ff7e086
t1mofey <tmfztsv27@gmail.com>
2024-06-20 20:57:18 +0300

    Выполнил все задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
+// Задача 1
+
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let discriminant = b * b - 4 * a * c;
+	if (discriminant < 0) {
+		return [];
+	} else if (discriminant === 0) {
+		let root = -b / (2 * a);
+		return [root];
+	} else {
+		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		return [root1, root2];
+	}
 }
+console.log(solveEquation(1, 5, 4))
+
+// Задача 2
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	if (typeof percent !== "number" || typeof contribution !== "number" || typeof amount !== "number" || typeof countMonths !== "number") {
+		return "Параметры содержат неправильное значение";
+	}
+
+	let monthlyInterest = percent / 100 / 12;
+	let loanBody = amount - contribution;
+	let monthlyPayment = loanBody * (monthlyInterest + monthlyInterest / (Math.pow(1 + monthlyInterest, countMonths) - 1));
+	let totalAmount = monthlyPayment * countMonths;
+
+	return parseFloat(totalAmount.toFixed(2));
 }
