df4ebc48943b2493937d42fcecb065f02dc70e93
t1mofey <tmfztsv27@gmail.com>
2024-06-27 11:05:03 +0300

    Сделал Задачу 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,17 @@
+//Задача 1
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  if (arr.length === 0) {
+    return { min: undefined, max: undefined, avg: undefined };
 }
 
+let min = Math.min(...arr);
+let max = Math.max(...arr);
+let sum = arr.reduce((acc, curr) => acc + curr, 0);
+let avg = sum / arr.length;
+  return { min, max, avg };
+}
+let params = getArrayParams(3, 5, 1, 9, 6);
+console.log(params);
 function summElementsWorker(...arr) {
 
 }
